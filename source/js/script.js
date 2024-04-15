f5586ff8602628b733fe4e87213b3dc812b5f31e
lalalolola <milenaallayarova.02@mail.ru>
2024-04-15 07:59:22 +0300

    Изменяет и добавляет js

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,6 @@ const buttonOpenNav = document.querySelector('.main-header__toggle');
 const nav = document.querySelector('.main-nav');
 
 buttonOpenNav.onclick = function () {
-  console.log("okokok")
   nav.classList.toggle('main-nav--opened');
   nav.classList.toggle('main-nav--closed');
 }
